7fd9bd763d8108451693d0bde9630df07482ef8e
Niclas Geisler <nicgeisler@protonmail.com>
2020-10-18 13:13:10 +0200

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TFT_Tracker.ViewModels;
 
 namespace TFT_Tracker
 {
@@ -23,6 +24,8 @@ namespace TFT_Tracker
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new MainViewModel();
         }
     }
 }
