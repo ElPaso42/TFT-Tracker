7db67e84a3aebb6814142faf7d588a9e2d992b92
Niclas Geisler <nicgeisler@protonmail.com>
2020-10-20 12:10:26 +0200

    Update MainViewModel.cs
    Add Command to switch View

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using TFT_Tracker.Commands;
 
 namespace TFT_Tracker.ViewModels
 {
@@ -15,5 +17,11 @@ namespace TFT_Tracker.ViewModels
             set { _selectedView = value; }
         }
 
+        public ICommand UpdateViewCommmand { get; set; }
+
+        public MainViewModel()
+        {
+            UpdateViewCommmand = new UpdateViewCommmand(this);
+        }
     }
 }
